de009c78e98b83bc79d1e9280dfdcfcd04f093b4
hasgdafga <hasgdafga@proton.me>
2024-03-06 07:58:11 +0800

    darak mod

---
+++ src/theme.tsx
@@ -0,0 +1,9 @@
+import { extendTheme, theme, ThemeConfig } from "@chakra-ui/react";
+
+const config: ThemeConfig = {
+  initialColorMode: "dark",
+};
+
+extendTheme({ config });
+
+export default theme;
