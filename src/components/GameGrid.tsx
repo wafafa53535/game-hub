819b9b968ac19475d3775c69d9970b2b8abf0585
hasgdafga <hasgdafga@proton.me>
2024-03-23 21:46:31 +0800

    Refactor gamegrid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import apiClient from "../Services/api-client";
 import useGames, { Platform } from "../hooks/useGames";
-import { SimpleGrid } from "@chakra-ui/react";
+import { SimpleGrid, Text } from "@chakra-ui/react";
 import GameCard from "./GameCard";
 import GamecardSkeleton from "./GamecardSkeleton";
 import GameCardContainer from "./GameCardContainer";
@@ -15,23 +15,22 @@ interface Props {
 const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isloading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
+
+  if (error) return <Text>{error}</Text>;
   return (
-    <>
-      {error && <div>{error}</div>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} padding="10px" spacing={6}>
-        {isloading &&
-          skeletons.map((skeleton) => (
-            <GameCardContainer key={skeleton}>
-              <GamecardSkeleton />
-            </GameCardContainer>
-          ))}
-        {data.map((game) => (
-          <GameCardContainer key={game.id}>
-            <GameCard game={game} />
+    <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} padding="10px" spacing={6}>
+      {isloading &&
+        skeletons.map((skeleton) => (
+          <GameCardContainer key={skeleton}>
+            <GamecardSkeleton />
           </GameCardContainer>
         ))}
-      </SimpleGrid>
-    </>
+      {data.map((game) => (
+        <GameCardContainer key={game.id}>
+          <GameCard game={game} />
+        </GameCardContainer>
+      ))}
+    </SimpleGrid>
   );
 };
 
